658cf938538beac5efda59b075123dde2602095a
tiberio40 <frenando40@hotmail.com>
2016-11-10 12:06:04 -0500

    Ultimo

---
+++ SREA.Tests/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SREA.Controllers
+{
+    [TestClass]
+    public class HomeController : Controller
+    {
+        [TestMethod]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [TestMethod]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [TestMethod]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
